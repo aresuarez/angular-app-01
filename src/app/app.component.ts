65808d46af30164ad622833a83ef7c066cbf1d52
aresuarez <supamumas@gmail.com>
2022-05-02 21:23:29 +0200

    First commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+})
+export class AppComponent {
+  titulo: string = 'Contador app';
+  numero: number = 10;
+  base: number = 5;
+
+  acumular(valor: number) {
+    this.numero += valor;
+  }
+}
